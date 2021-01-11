8b644af2b5791916abd0237a634f2780ba9402fa
amadrocky <ak45amad@hotmail.fr>
2021-01-11 03:15:45 +0100

    configuraion stripe subscriptions

--- src/Controller/PaymentController.php
+++ src/Controller/PaymentController.php
@@ -11,54 +11,83 @@ use App\Entity\Order\Order;
 class PaymentController extends AbstractController
 {
     /**
-     * @Route("/payment/{email}/{total}", name="payment", methods={"GET"})
-     *
-     * @param string $email
-     * @param integer $total
+     * @Route("/payment", name="payment", methods={"GET"})
      * @return Response
      */
-    public function index(string $email, int $total): Response
+    public function index(): Response
     {
-        return $this->render('payment/index.html.twig', [
-            'email' => $email,
-            'total' => $total
-        ]);
+        return $this->render('payment/index.html.twig');
     }
 
     /**
-     * @Route("/payment/process", name="payment_process", methods={"POST"})
+     * @Route("/payment/process", name="payment_process", methods={"GET", "POST"})
      *
      * @return Response
      */
-    public function process(): Response
+    public function paymentProcess(): Response
     {
-        require_once('vendor/autoload.php');
+        dd($_GET, $_POST);
 
-        $stripe = [
-            "secretkey" => "sk_test_51I7qCgIjktDIYiezUfNYo411jpXTPey9JPxQBzojqxMJxHKmUA6XN2czkq5r4dGieTTSZytFtYosvhLReG1m3z3E00GDzfPTIn",
-            "publishable_key" => "pk_test_51I7qCgIjktDIYiezyk245q27khXTpXCzXgu9AMx3A6n1ay8U81Ap7Rt8EpMUwu9kk9qWC2QC5Ymi0MJ9eJahAiBR00gqYitQ6F"
-        ];
+        $stripe = new \Stripe\StripeClient(
+            'sk_test_51I7qCgIjktDIYiezUfNYo411jpXTPey9JPxQBzojqxMJxHKmUA6XN2czkq5r4dGieTTSZytFtYosvhLReG1m3z3E00GDzfPTIn'
+          );
 
-        \Stripe\Stripe::setApikey($stripe['secret_key']);
+        $period = $_GET['period'];
+        $email = $_POST['email'];
 
-        $period = $_POST['period'];
-        $token = $_POST['stripetoken'];
-        $email = $_POST['stripeEmail'];
-
-        $customer = \Stripe\Customer::create([
+        $customer = $stripe->customers->create([
+            'description' => 'New Customer (created for API docs)',
             'email' => $email,
-            'source' => $token,
         ]);
-
-        \Stripe\Subscription::create([
-            "customer" => $customer->id,
-            "items" => [
-                "plan" => "price_1I7sM3IjktDIYiezn6kNu6sh"
+        
+        $payment = $stripe->paymentMethods->create([
+            'type' => 'card',
+            'card' => [
+              'number' => '4242424242424242',
+              'exp_month' => 1,
+              'exp_year' => 2022,
+              'cvc' => '314',
             ],
         ]);
 
+        $stripe->paymentMethods->attach(
+            $payment->id,
+            ['customer' => $customer->id]
+        );
+
+        $updatedCustomer = $stripe->customers->update(
+            $customer->id,
+            ['invoice_settings' => ['default_payment_method' => $payment->id]]
+          );
+        
+        if (intval($period) === 6) {
+            $subscription = $stripe->subscriptions->create([
+                'customer' => $updatedCustomer->id,
+                'items' => [
+                    ['price' => 'price_1I7sM3IjktDIYiezn6kNu6sh'],
+                ],
+            ]);
+        } elseif (intval($period) === 12) {
+            $subscription = $stripe->subscriptions->create([
+                'customer' => $updatedCustomer->id,
+                'items' => [
+                    ['price' => 'price_1I7sUSIjktDIYiez9FXqCMHS'],
+                ],
+            ]);
+        } else {
+            $subscription = $stripe->subscriptions->create([
+                'customer' => $updatedCustomer->id,
+                'items' => [
+                    ['price' => 'price_1I7sVlIjktDIYiezv2ni8asi'],
+                ],
+            ]);
+        }
+        
+
+        $this->addflash('success', 'Subsciption activated');
+
         return $this->render('payment/index.html.twig', [
-            
+            'email' => $email,
         ]);
     }
 }
